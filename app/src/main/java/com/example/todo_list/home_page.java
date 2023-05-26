dffcd04662ad7f6604d460c6700c94943dc2a7c6
Onthemood20195445 <73796263+Onthemood20195445@users.noreply.github.com>
2023-05-26 16:48:31 +0300

    update

--- app/src/main/java/com/example/todo_list/home_page.java
+++ app/src/main/java/com/example/todo_list/home_page.java
@@ -3,11 +3,23 @@ package com.example.todo_list;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class home_page extends AppCompatActivity {
+    ImageView addNote;
+    ListView list;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> tasks;
+    EditText input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +30,34 @@ public class home_page extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(home_page.this, sign_in.class);
                 startActivity(intent);
+            }
+        });
+        addNote = findViewById(R.id.add_item);
+        tasks = new ArrayList<>();
+        input = findViewById(R.id.inp);
+        list  = findViewById(R.id.listView);
+        adapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1,tasks);
+        list.setAdapter(adapter);
+
+
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                String txt = input.getText().toString();
+                if(txt == null || txt.length()==0){
+                    String msg = "Please Enter An Item";
+                    Toast toast = Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else {
+                    tasks.add(txt);
+                    String msg = txt+" is Added";
+                    Toast toast = Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_SHORT);
+                    toast.show();
+                    input.setText("");
+                    list.setAdapter(adapter);
+                }
             }
         });
 
