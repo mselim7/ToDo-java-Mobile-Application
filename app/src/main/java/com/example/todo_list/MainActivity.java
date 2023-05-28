fabfd751873a1587b3292038ecdf95d29d7fb66e
Onthemood20195445 <73796263+Onthemood20195445@users.noreply.github.com>
2023-05-28 10:51:37 +0300

    update
    fixed home and added function done task and checkbutton and delet all icon

--- app/src/main/java/com/example/todo_list/MainActivity.java
+++ app/src/main/java/com/example/todo_list/MainActivity.java
@@ -10,21 +10,128 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
-Button loginpage;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity implements TodoAdapter.OnTodoItemClickListener {
+    private List<TodoItem> items;
+    private TodoAdapter adapter;
+    private RecyclerView recyclerView;
+    ImageView addNote;
+    EditText input;
+    public List<TodoItem> items2;
+    public TodoAdapter adapter2;
+    public RecyclerView recyclerView2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(MainActivity.this, sign_in.class);
-        startActivity(intent);
-        loginpage = findViewById(R.id.Login_page);
-        loginpage.setOnClickListener(new View.OnClickListener() {
+
+        items = new ArrayList<>();
+        input = findViewById(R.id.inp);
+        addNote = findViewById(R.id.add_item);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new TodoAdapter(items, this, this);
+        recyclerView.setAdapter(adapter);
+
+        items2= new ArrayList<>();
+        recyclerView2 = findViewById(R.id.recycler_views);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(this));
+        adapter2 = new TodoAdapter(items2, this,this);
+        recyclerView2.setAdapter(adapter2);
+
+        ImageView btn = findViewById(R.id.clearAllBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                items.clear();
+                adapter.notifyDataSetChanged();
+            }
+        });
+        Button logout = findViewById(R.id.logoutbtn);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), sign_in.class);
+                startActivity(intent);
+            }
+        });
 
+
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt = input.getText().toString();
+                if (txt == null || txt.length() == 0) {
+                    String msg = "Please Enter An Item";
+                    Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    addItem(txt);
+                    String msg = txt + " is Added";
+                    Toast toast = Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT);
+                    toast.show();
+                    adapter.notifyDataSetChanged();
+                    input.setText("");
+                    InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
             }
         });
     }
+
+    @Override
+    public void onItemClick(int position) {
+        TodoItem item = items.get(position);
+        item.setChecked(!item.isChecked());
+        adapter.notifyItemChanged(position);
+    }
+
+    @Override
+    public void onItemLongClick(int position) {
+        deleteItem(position);
+    }
+
+    public void addItem(String text) {
+        TodoItem item = new TodoItem(text);
+        items.add(item);
+        adapter.notifyItemInserted(items.size() - 1);
+    }
+
+    public void addItem2(String text) {
+        TodoItem newItem = new TodoItem(text);
+        newItem.setChecked2(true); // set isChecked2 to true by default
+        items2.add(newItem);
+        adapter2.notifyItemInserted(items2.size() - 1);
+    }
+
+    public void deleteItem(int position) {
+        items.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+    public void clearAll() {
+        items.clear();
+        adapter.notifyDataSetChanged();
+    }
+
+
 }
---
+++ app/src/main/java/com/example/todo_list/TodoAdapter.java
@@ -0,0 +1,88 @@
+package com.example.todo_list;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder> {
+    public List<TodoItem> items;
+    public OnTodoItemClickListener listener;
+    public MainActivity activity;
+
+    public interface OnTodoItemClickListener {
+        void onItemClick(int position);
+        void onItemLongClick(int position);
+    }
+
+    public TodoAdapter(List<TodoItem> items, MainActivity activity, OnTodoItemClickListener listener) {
+        this.items = items;
+        this.listener = listener;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public TodoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_view_item, parent, false);
+        return new TodoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TodoViewHolder holder, int position) {
+        TodoItem item = items.get(position);
+        holder.checkBox.setChecked(item.isChecked());
+
+        if (activity.items2.contains(item)) {
+            holder.checkBox.setChecked(item.isChecked2()); //use isChecked2 for item2 list
+        } else {
+            holder.checkBox.setChecked(item.isChecked()); // use isChecked for items list
+        }
+
+        holder.textView.setText(item.getText());
+
+        holder.checkBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            item.setChecked(isChecked);
+            if (isChecked) {
+                Toast.makeText(buttonView.getContext(), item.getText()+" Done", Toast.LENGTH_SHORT).show();
+                activity.addItem2(item.getText());
+                activity.deleteItem(position);
+            }
+        });
+
+
+        holder.itemView.setOnLongClickListener(v -> {
+            if (activity.items2.contains(item)) {
+
+
+            } else {
+                listener.onItemLongClick(position);
+            }
+            return true;
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class TodoViewHolder extends RecyclerView.ViewHolder {
+        public CheckBox checkBox;
+        public TextView textView;
+
+        public TodoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            checkBox = itemView.findViewById(R.id.checkbox);
+            textView = itemView.findViewById(R.id.texts);
+        }
+    }
+}
